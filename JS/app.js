a8926d735d8d51139e2834735c6a1784f1e976df
sorkinfork <levykin.web@gmail.com>
2023-02-06 14:41:38 +0300

    Арифметические операторы

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,15 @@
-console.log ('Hello World!');
+// Базовые арифметические операторы
+const width = 10;
+const height = 5;
+const space = 10 * 5;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 * 2 * 2; // 2 ** 3
+console.log (volume);
+
+
+// Строки
+const city = 'Москва';
+const street = 'Новослободская';
+console.log (city + ', ' + street + ' ' + 5);
