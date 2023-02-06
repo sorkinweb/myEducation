5d252a08516971ce8092d0939750cab5884c35b4
sorkinfork <levykin.web@gmail.com>
2023-02-06 15:09:58 +0300

    Oператоры присваивания и сравнения

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,21 @@
-// Базовые арифметические операторы
-const width = 10;
-const height = 5;
-const space = 10 * 5;
-const newWidth = width - 4;
-const newWidth2 = width + 4;
-const division = newWidth / newWidth2;
-const volume = 2 * 2 * 2; // 2 ** 3
-console.log (volume);
+// Операторы присваивания
+
+let age = 18 + 5;
+age += 2; // age = age + 2
+age -= 3; // age = age - 2
+age *= 2; // age = age * 2
+age /= 2; // age = age / 2
+
+age++; // age = age + 1
+age--; // age = age -1 
 
 
-// Строки
-const city = 'Москва';
-const street = 'Новослободская';
-console.log (city + ', ' + street + ' ' + 5);
+console.log (age);
+
+// Операторы сравнения
+const vasia = 20;
+console.log (age > vasia);
+console.log (age >= vasia);
+console.log (age < vasia);
+console.log (age <= vasia);
+console.log (age == vasia);
