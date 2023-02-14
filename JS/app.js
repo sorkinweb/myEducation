833f9f86c5cdc7e7b238e75f5a771f222f7262c3
sorkinfork <levykin.web@gmail.com>
2023-02-14 09:52:29 +0300

    Особенности функций

--- JS/app.js
+++ JS/app.js
@@ -1,22 +1,45 @@
-// Упражнение. Упрощение функции
+// Параметры по умолчанию
 
-function toPower(num, power) {
+function toPower(num, power = 2) {
     const res = num ** power;
     return res;
 }
 
-console.log(toPower (2, 3));
+console.log(toPower(2, 3));
+console.log(toPower(2));
 
-// Переписать в стрелочную функцию.
-// Мой вариант
+// Условия в функциях
 
-const toPowerArrow = (num, power) => {
-    return num ** power;
+function canAccesWebsite(age) {
+    if (age < 18) {
+        return 'Нет';
+    }
+        return 'Да';
 }
 
-console.log(toPowerArrow(2, 3));
+console.log(canAccesWebsite(16));
 
-// Вариант Антона
+const canAccesWebsite2 = age => age < 18 ? 'Нет' : 'Да';
+console.log(canAccesWebsite2(16));
 
-const toPowerArrow1 = (num, power) => num ** power;
-console.log(toPowerArrow1(2, 3));
+// Функции в функциях
+
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
+
+function calculateW(present) {
+    return present * KG_IN_USD;
+}
+
+function calculateKm( distance) {
+    return  distance * KM_IN_USD;
+}
+
+function getExchangePrice(present1, present2, distance) {
+    const price1 = calculateW(present1);
+    const price2 = calculateW(present2);
+    const distancePrice = calculateKm(distance)
+    return price1 + price2 + distancePrice;
+}
+
+console.log(getExchangePrice(1,2,10));
