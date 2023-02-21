ab364a70ce191e3105f648a8e8c04ebd5355c233
sorkinfork <levykin.web@gmail.com>
2023-02-21 16:50:17 +0300

    Принцип DRY

--- JS/app.js
+++ JS/app.js
@@ -1,14 +1,7 @@
+// Принцип DRY. Don`t Repeat Yourself
+// Если мы повторяем какую-то часть кода, то это повод задуматься о том, что, возможно, эту часть кода нужно вынести в отдельную функцию.
 
-/*
-    Упражнение. Обновление списка задач
-    Дан список задач
-    const tasks = ['Задача 1'];
-    Сделать функции:
-    - Добавление задачи в конец
-    - Удаление задачи по названию
-    - Перенос задачи в начало списка по названию
-    Всегда меняем исходный массив
-*/
+
 const tasks = ['Задача 1'];
 
 function addTask (task) {
@@ -16,28 +9,27 @@ function addTask (task) {
 }
 
 function deleteTask (task) {
-    const index = tasks.indexOf(task)
+    const index = tasks.indexOf(task);
     if (index === -1) {
         return;
     }
-    tasks.splice(index,1);
+    return tasks.splice(index,1);
 }
 
 function transferTask (task) {
-    const index = tasks.indexOf(task)
-     if (index === -1) {
+    const result = deleteTask(task);
+    if (!result) { 
         return;
-    }
-    tasks.splice(index,1);
-    tasks.unshift(task);
+    }   
+        tasks.unshift(result[0]);
 }
 
 addTask('Задача 2');
 addTask('Задача 3');
 console.log(tasks);
 
-deleteTask('Задача 4');
+deleteTask('Задача 2');
 console.log(tasks);
 
-transferTask('Задача 4');
+transferTask('Задача 3');
 console.log(tasks);
