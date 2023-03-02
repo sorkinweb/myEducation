199e97f0e176520d1782088881c64e7a298ce76d
sorkinfork <levykin.web@gmail.com>
2023-03-02 15:50:22 +0300

    Цикл while

--- JS/app.js
+++ JS/app.js
@@ -1,20 +1,32 @@
-// Цикл в цикле
+// Цикл while
 
-// for (let i = 1; i < 5; i ++) {
-//     console.log(`Цикл 1 - ${i}`);
-//     for (let j = 1; j < 5; j ++) {
-//     console.log(`Цикл 2 - ${j}`);
-//     }
+for (let i = 1; i < 5; i++) {
+    console.log(`Вывод - ${i}`);
+} 
+
+// let i = 1;
+// while (i < 5) {
+//     console.log(`Вывод - ${i}`);
+//     i++;
 // }
 
-// Вложенный цикл будет полностью итерироваться каждый раз!
+const arr = [1, 4, 8, 7];
 
-// Массив в массиве
-
-const tasks = [[1, 'Задача 1'], [2, 'Задача 2']];
-
-for (let i = 0; i < tasks.length; i ++) {
-    for (let j = 0; j < tasks[i].length; j ++) {
-        console.log(tasks[i] [j]);
+for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 5) {
+        break;
     }
+    console.log(arr[i]); // 1, 4
+} 
+
+let i = 0;
+while (arr[i] <= 5  && i < arr.length) {
+    console.log(arr[i]); // 1, 4
+    i++;
 }
+
+let j = 0;
+do {
+    console.log(j); // 0
+    j++
+} while (j < 0);
