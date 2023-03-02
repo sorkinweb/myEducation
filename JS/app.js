f6376c89c4f3baff625f1bcead1a476aef59f450
sorkinfork <levykin.web@gmail.com>
2023-03-02 15:26:36 +0300

    Цикл в цикле

--- JS/app.js
+++ JS/app.js
@@ -1,17 +1,20 @@
-/*
-    Задача вывести в консоль строчку "Я люблю JS !" из массива,
-    проходя циклом в обратном порядке, не используя метод reverse.
+// Цикл в цикле
 
-*/
+// for (let i = 1; i < 5; i ++) {
+//     console.log(`Цикл 1 - ${i}`);
+//     for (let j = 1; j < 5; j ++) {
+//     console.log(`Цикл 2 - ${j}`);
+//     }
+// }
 
-const arr = ['!', 'JS', 'люблю', 'Я'];
-const resultArray = [];
+// Вложенный цикл будет полностью итерироваться каждый раз!
 
-for(let el = arr.length - 1; el >= 0; el --) {
-    resultArray.push(arr[el]);
+// Массив в массиве
+
+const tasks = [[1, 'Задача 1'], [2, 'Задача 2']];
+
+for (let i = 0; i < tasks.length; i ++) {
+    for (let j = 0; j < tasks[i].length; j ++) {
+        console.log(tasks[i] [j]);
+    }
 }
-console.log(resultArray.join(' '));
-
-// через reverse
-
-console.log(arr.reverse().join(' '));
