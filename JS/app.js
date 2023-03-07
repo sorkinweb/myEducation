548100801471bddf5f781dfc39e7fc17bc1bb2af
sorkinfork <levykin.web@gmail.com>
2023-03-07 17:01:42 +0300

    Упражнение. Расчет итогового баланса

--- JS/app.js
+++ JS/app.js
@@ -1,15 +1,65 @@
-// Циклы for of и for in
+// Упражнение. Расчет итогового баланса
 
-const arr = [1, 4, 8, 7];
+/*
+    Есть выгрузка операций пользователя
+    const operations = [1000, -700, 300, -500, 10000];
+    а так же начальный баланс в 100$
+    Необходимо сделать функции расчета:
+    - Итогового баланса
+    - Наличия отрицательного баланса (если после очередной операции баланс < 0, то выдавать false)
+    - Расчета среднего расхода и среднего дохода  
+*/
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+const operations = [1000, -700, 300, -500, 10000];
+const startingBalance = 100;
+
+// Итоговый баланс
+function getBalance(arrayofOperations, initialBalance) {
+    let balance = initialBalance; 
+    for (const element of arrayofOperations) {
+        balance += element;
+    }
+        return balance;
 }
 
-for (let element of arr) { // чтобы перебрать целиком весь массив и сделать какую операцию с ним (итерируется по значениям)
-    console.log(element);
+console.log(getBalance(operations, startingBalance)); // 10200
+
+// Наличие отрицательного баланса
+
+function checkOperations(arrayofOperations, initialBalance) {
+    let balance = initialBalance;
+    let isOk = true;
+    for (const element of arrayofOperations) {
+        balance += element;
+        if (balance < 0) {
+            isOk = false;
+            break;
+        }
+    }
+    return isOk;
 }
 
-for (let index in arr) { // итерируется по индексам массива
-    console.log(index);
+console.log(checkOperations(operations, startingBalance)); // true
+
+
+// Расчет среднего дохода / расхода
+
+function avarageOperations (arrayofOperations) {
+    let positiveCount = 0;
+    let positiveSum = 0;
+    let negativeCount = 0;
+    let negativeSum = 0;
+    for (const element of arrayofOperations) {
+        if (element > 0) {
+            positiveCount ++;
+            positiveSum += element;
+        }
+                if (element < 0) {
+            negativeCount ++;
+            negativeSum += element;
+        }
+    }
+    return [positiveSum / positiveCount, negativeSum / negativeCount];
 }
+
+console.log(avarageOperations(operations));
